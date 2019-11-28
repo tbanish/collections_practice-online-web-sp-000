78c5f28bc899231eb63f33a51b69d819f115e264
Tom Banish <tombanish@gmail.com>
2019-11-28 23:27:30 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -2,6 +2,7 @@ def sort_array_asc(array)
   array.sort
 end
 
+
 def sort_array_desc(array)
   array.sort do |a, b|
     b <=> a
@@ -9,5 +10,9 @@ def sort_array_desc(array)
 end
   
 
-
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    a.length <=> b.length
+  end
+end
 
