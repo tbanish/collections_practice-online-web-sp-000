f75cdf1352e6e0d05bdb88241e40eff0f277b34a
Tom Banish <tombanish@gmail.com>
2019-11-29 00:03:46 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -48,7 +48,9 @@ end
 
 def add_s(array)
   array.each_with_index.collect do |element, index|
-    if index != 1
+    if index = 1
+      return element
+    else  
       element << "s"
     end
   end
